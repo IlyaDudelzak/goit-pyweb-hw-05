22691291037ea43e999448b1db212787f881d777
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2024-12-15 13:28:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 from sys import argv
 import platform
 import aiohttp
 import asyncio
 
 days=2
+currencies = ["EUR", "USD"]
 
-try:
-    if(int(argv[1]) < 1 or int(argv[1]) > 10):
-        print("Wrong day count argument! Use between 1 and 10 (default 2)")
+async def date_list(start, length, frwd=True):
+    for i in range(length):
+        delta = timedelta(days=i)
+        if(frwd):
+            yield start + delta
+        else:
+            yield start - delta
+    raise StopIteration
+async def date_to_string(date):
+    return date.strftime("%d.%m.%Y")
+
+async def check_arguments():
+    try:
+        if(int(argv[1]) < 1 or int(argv[1]) > 10):
+            print("Wrong day count argument! Use between 1 and 10 (default 2)")
+            exit()
+
+    except ValueError:
+        print("Day count is not an integer!")
+        exit()
+    
+    if(len(argv) > 1):
+        global days
+        days = int(argv[1])
+
+async def API_request(URL:str, date:datetime):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(str.replace(URL, "%DATE%", await date_to_string(date))) as response:
+                return await response.json()
+    except aiohttp.client_exceptions.ClientConnectorDNSError:
+        print("Cannot connect to the server!")
         exit()
         
-except ValueError:
-    print("Day count is not an integer!")
-    exit()
-
-days = int(argv[1])
+async def data_collector():
+    currency_data = {}
+    async with aiohttp.ClientSession() as session:
+        async for date in date_list(datetime.now(), days, False):
+            data = (await API_request("https://api.privatbank.ua/p24api/exchange_rates?json&date=%DATE%", date))["exchangeRate"]
+            current = {}
+            for currency in data:
+                if(currency["currency"] in currencies):
+                    current[currency["currency"]] = {"sale": currency["saleRate"], "purchase": currency["purchaseRate"]}
+            currency_data[await date_to_string(date)] = current
+                    
+    return currency_data
 
 async def main():
-    res = {}
-    async with aiohttp.ClientSession() as session:
-        today = datetime.now()
-        for i in range(days):
-            date = (today - timedelta(days=i)).strftime("%d.%m.%Y")
-            async with session.get(f"https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5&date={date}") as response:
-                res[date] = await response.json()
-    return res
+    await check_arguments()
+    print(await data_collector())
 
 if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    print(r)
+    asyncio.run(main())
  
